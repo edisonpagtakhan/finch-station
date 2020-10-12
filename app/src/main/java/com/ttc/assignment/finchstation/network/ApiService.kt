d5e6f75c7797baf05c0bba2b2015c3097ee78ecb
Edison Pagtakhan <edisonpagtakhan03@gmail.com>
2020-10-12 23:00:04 +0800

    feat: Add finch station endpoint to ApiService

--- app/src/main/java/com/ttc/assignment/finchstation/network/ApiService.kt
+++ app/src/main/java/com/ttc/assignment/finchstation/network/ApiService.kt
@@ -1,8 +1,12 @@
 package com.ttc.assignment.finchstation.network
 
+import com.ttc.assignment.finchstation.data.Station
+import retrofit2.http.GET
+
 /**
  * Created by edison on 10/12/20.
  */
 interface ApiService {
-
+    @GET("finch_station.json")
+    suspend fun getFinchStationData(): Station
 }
