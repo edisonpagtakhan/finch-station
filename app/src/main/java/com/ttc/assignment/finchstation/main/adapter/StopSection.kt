86cb6af96925d26eee3aad04bd2996f0b084aa75
Edison Pagtakhan <edisonpagtakhan03@gmail.com>
2020-10-14 00:04:48 +0800

    feat: Add StopSection class for RecyclerView

---
+++ app/src/main/java/com/ttc/assignment/finchstation/main/adapter/StopSection.kt
@@ -0,0 +1,52 @@
+package com.ttc.assignment.finchstation.main.adapter
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ttc.assignment.finchstation.R
+import com.ttc.assignment.finchstation.data.Route
+import com.ttc.assignment.finchstation.data.Stop
+import io.github.luizgrp.sectionedrecyclerviewadapter.Section
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionParameters
+import kotlinx.android.synthetic.main.route_item.view.*
+import kotlinx.android.synthetic.main.stop_header.view.*
+
+/**
+ * Created by edison on 10/13/20.
+ */
+class StopSection(private val stop: Stop) : Section(
+    SectionParameters.builder()
+        .headerResourceId(R.layout.stop_header)
+        .itemResourceId(R.layout.route_item)
+        .build()) {
+
+    override fun getContentItemsTotal(): Int = stop.routes.size
+
+    override fun getHeaderViewHolder(view: View): RecyclerView.ViewHolder = HeaderViewHolder(view)
+
+    override fun onBindHeaderViewHolder(holder: RecyclerView.ViewHolder) {
+        (holder as HeaderViewHolder).bindData(stop.name)
+    }
+
+    override fun getItemViewHolder(view: View): RecyclerView.ViewHolder = RouteViewHolder(view)
+
+    override fun onBindItemViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as RouteViewHolder).bindData(stop.routes[position])
+    }
+
+    class HeaderViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val headerView: TextView = view.headerView
+
+        fun bindData(stopName: String) {
+            headerView.text = stopName
+        }
+    }
+
+    class RouteViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        private val routeTextView: TextView = view.routeTextView
+
+        fun bindData(route: Route) {
+            routeTextView.text = route.name
+        }
+    }
+}
