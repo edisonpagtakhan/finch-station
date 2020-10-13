12d37c3e21b5dadcbac04240a09f13f79b634938
Edison Pagtakhan <edisonpagtakhan03@gmail.com>
2020-10-14 00:23:17 +0800

    feat: Add initial layout for MainActivity

--- app/src/main/java/com/ttc/assignment/finchstation/main/FinchStationViewModel.kt
+++ app/src/main/java/com/ttc/assignment/finchstation/main/FinchStationViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.ttc.assignment.finchstation.data.Station
+import com.ttc.assignment.finchstation.data.Stop
 import com.ttc.assignment.finchstation.network.RetrofitClient
 import kotlinx.coroutines.launch
 
@@ -13,19 +14,35 @@ import kotlinx.coroutines.launch
  */
 class FinchStationViewModel: ViewModel() {
 
+    private var station: Station? = null
+
     private val loading: MutableLiveData<Boolean> by lazy {
         MutableLiveData<Boolean>(false)
     }
 
-    private val stationResp: MutableLiveData<Station> by lazy {
-        MutableLiveData<Station>()
+    private val mutableStationName: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
+
+    private val mutableStops: MutableLiveData<List<Stop>> by lazy {
+        MutableLiveData<List<Stop>>()
+    }
+
+    private val showAllStops: MutableLiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>(false)
     }
 
     val isLoading: LiveData<Boolean>
         get() = loading
 
-    val station: LiveData<Station>
-        get() = stationResp
+    val stationName: LiveData<String>
+        get() = mutableStationName
+
+    val stops: LiveData<List<Stop>>
+        get() = mutableStops
+
+    val willShowAllStops: LiveData<Boolean>
+        get() = showAllStops
 
     fun requestData() {
         loading.value = true
@@ -38,11 +55,24 @@ class FinchStationViewModel: ViewModel() {
                     .getFinchStationData()
 
                 loading.value = false
-                stationResp.value = response
+                station = response
+
+                mutableStationName.value = station!!.name
+                updateStops()
 
             } catch (exception: Exception) {
                 loading.value = false
             }
+        }
+    }
+
+    private fun updateStops() {
+        val stops = station!!.stops
+
+        mutableStops.value = if (showAllStops.value!!) {
+            stops
+        } else {
+            stops.filter { it.routes.isNotEmpty() }
         }
     }
 }
--- app/src/main/java/com/ttc/assignment/finchstation/main/MainActivity.kt
+++ app/src/main/java/com/ttc/assignment/finchstation/main/MainActivity.kt
@@ -1,20 +1,38 @@
 package com.ttc.assignment.finchstation.main
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.observe
 import com.ttc.assignment.finchstation.R
+import com.ttc.assignment.finchstation.databinding.ActivityMainBinding
+import com.ttc.assignment.finchstation.main.adapter.StopSection
+import io.github.luizgrp.sectionedrecyclerviewadapter.SectionedRecyclerViewAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private val viewModel: FinchStationViewModel by viewModels()
+    private lateinit var dataBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        dataBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        configureViews()
         initializeViewModel()
+    }
+
+    private fun configureViews() {
+        setSupportActionBar(toolbar)
+        configureRecyclerView()
+    }
+
+    private fun configureRecyclerView() {
+        stopsRecyclerView.adapter = SectionedRecyclerViewAdapter()
+        //TODO: Add divider
     }
 
     private fun initializeViewModel() {
@@ -27,8 +45,24 @@ class MainActivity : AppCompatActivity() {
                 //TODO: Handle loading
             }
 
-            station.observe(owner = lifeCycleOwner) { station ->
+            stationName.observe(owner = lifeCycleOwner) { stationName ->
+                supportActionBar!!.title = stationName
+            }
 
+            stops.observe(owner = lifeCycleOwner) { stops ->
+                val adapter = stopsRecyclerView.adapter as SectionedRecyclerViewAdapter
+
+                stops.forEach { stop ->
+                    val section = StopSection(stop)
+
+                    adapter.addSection(section)
+                }
+
+                adapter.notifyDataSetChanged()
+            }
+
+            willShowAllStops.observe(owner = lifeCycleOwner) { willShow ->
+                dataBinding.isChecked = !willShow
             }
         }
 
